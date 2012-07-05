7cd041ee3173d0c3fab4dde9fcb92a3c6c72860c
Romain Gérard <romaingweb@gmail.com>
2012-07-05 22:07:51 +0200

    Design application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  helper_method :days_left
   protect_from_forgery
+
+  def days_left
+    @days_left = (Date.new(2012, 10, 26) - Date.today).to_i
+  end
 end
