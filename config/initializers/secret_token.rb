c2f4aa34186e466ff46c1ebdab1acc80230cd00e
Romain Gérard <romaingweb@gmail.com>
2012-07-05 20:28:48 +0200

    Create rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atbru::Application.config.secret_token = '3b24c5eaa62c8e6e8910775bed99a945454ca565a4577e45f956648e156b87762c84218aac9e5dbd1ac287e3eeb46b26d6173c726a4896abd815fff60c19a564'
