8897a0b47b6368bafc848863041675cd4e426309
Daniel Reszka <dannn.r@gmail.com>
2013-09-10 00:16:44 +0200

    Update program with pracitioner labels, improve existing labels, complete path descriptions

---
+++ app/helpers/labels_helper.rb
@@ -0,0 +1,29 @@
+#encoding: UTF-8
+module LabelsHelper
+
+  def label type
+
+      title = case type
+              when :beginner then 'Beginner friendly'
+              else type.to_s
+              end
+      text =  case type
+              # when :beginner then '★☆☆ Beginner'
+              when :beginner then '★ Beginner'
+              when :practitioner then '★★ Practitioner'
+              when :expert then '★★★ Expert'
+              else type.to_s
+              end
+    content_tag :span, class: [ :label, :"label-#{type}" ], title: title do
+      text
+    end
+  end
+
+  def labels *labels
+    labels = Array(labels)
+    labels.map!{ |l| label l }
+    content_tag :div, class: [ :labels ] do
+      labels.join.html_safe
+    end
+  end
+end
